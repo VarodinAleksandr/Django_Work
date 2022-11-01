a7e0d4d9f1747a69753d8a8382121caf033ce958
VarodinAleksandr <olisgroup21@gmail.com>
2022-11-01 17:25:04 +0200

    model form

--- triangle/models.py
+++ triangle/models.py
@@ -1,3 +1,10 @@
-from django.db import models  # noqa : F401
+from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
--- triangle/urls.py
+++ triangle/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import calculate_triangle
-
+from .views import calculate_triangle, person, update_person
 
 app_name = 'triangle'
 urlpatterns = [
     path('', calculate_triangle, name='calculation'),
+    path('person', person, name='person'),
+    path('person/<int:pk>', update_person, name='update_person'),
 ]
